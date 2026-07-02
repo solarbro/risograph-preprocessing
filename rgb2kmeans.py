dc29c88397834b023a7afdde4e5b2205ceb4e7de
solarbro <sgnkchowdhury@gmail.com>
2026-07-02 13:00:43 -0700

    Added filter pass to prevent excessive overpainting

--- rgb2kmeans.py
+++ rgb2kmeans.py
@@ -6,15 +6,20 @@ import math
 from skimage import color
 from enum import Enum
 
+# TODO: 
+# [ ] In gradient mode, keep the pixels for the top N layers, and discard the rest (set to white)
+# [ ] Normalize layer weights so that overlapping layers sum to 1. This should apply after the pixel discard pass
+
 class Mode(Enum):
     Gradient = 0
     Bucket   = 1
 
 class Options:
-    def __init__(self, k: int, mode: Mode, alpha: float = 1., invert: bool = False):
+    def __init__(self, k: int, mode: Mode, max_layers: int, alpha: float = 1., invert: bool = False):
         self.k = k
         self.mode = mode
         if self.mode == Mode.Gradient:
+            self.max_layers = max_layers
             self.alpha = alpha
             self.invert = invert
 
@@ -45,6 +50,24 @@ def save_image(layer, path):
     img = PIL.Image.fromarray(np.squeeze(img, axis=-1))
     img.save(path)
 
+# TODO: This isn't working the way I need it to
+def keep_top_n_layers(layers: list[np.ndarray], n: int, renormalize: bool) -> list[np.ndarray]:
+    print(f'Keep top {n} weights')
+    # (K, H, W)
+    weights = np.stack(layers, axis=0).squeeze(-1)
+    print(f'Shape of weights: {weights.shape}')
+    # Find the top N layer indices for each pixel
+    top = np.argpartition(weights, n - 1, axis=0)[:n]
+    # Discard bottom k - n layers
+    filtered = np.ones_like(weights)
+    y, x = np.indices(weights.shape[1:])
+    np.put_along_axis(filtered, top, np.take_along_axis(weights, top, axis=0), axis=0)
+    if renormalize:
+        filtered /= np.maximum(filtered.sum(axis=0, keepdims=True), 1e-8)
+    # Restore the trailing singleton dimension
+    filtered = np.expand_dims(filtered, axis=-1)
+    return list(filtered)
+
 def rgb2kmeans(input_path, options: Options, output_prefix):
     img = load_image(input_path)
     img = (img / 255.).astype(np.float32)
@@ -69,9 +92,13 @@ def rgb2kmeans(input_path, options: Options, output_prefix):
             # Divide by an arbitrary max value to "normalize"
             l /= 200.
             l = np.clip(l * options.alpha, 0, 1)
-            if options.invert:
-                l = 1 - l
             layers.append(l.reshape((img.shape[0], img.shape[1], 1)))
+        # Keep top N layers only and renormalize the weights of the remaining layers
+        layers = keep_top_n_layers(layers, options.max_layers, False)
+        # Invert weights
+        if options.invert:
+            for l in layers:
+                l = 1 - l
     elif options.mode == Mode.Bucket:
         labels = model.predict(pixels)
         for i in range(options.k):
@@ -110,9 +137,10 @@ if __name__=='__main__':
     parser.add_argument('-i', '--inverse', action='store_true', help='Invert layer weights')
     parser.add_argument('-a', '--alpha', type=float, default=1., help='Scaling factor for distance function. Used with gradient mode.')
     parser.add_argument('-b', '--bucket', action='store_true', help='Use bucketing mode instead of gradient. Results in distinct layers with flat colors instead of overlapping gradients.')
+    parser.add_argument('-l', '--max_overlapping_layers', default=2, help='Maximum number of overlapping gradients per pixel.')
     args = parser.parse_args()
     if args.bucket:
         options = Options(args.num_centroids, Mode.Bucket)
     else:
-        options = Options(args.num_centroids, Mode.Gradient, args.alpha, args.inverse)
+        options = Options(args.num_centroids, Mode.Gradient, args.max_overlapping_layers, args.alpha, args.inverse)
     rgb2kmeans(args.input, options, args.prefix)
