15131e00640c01e2ce634c27409f30b4fdd1a186
solarbro <sgnkchowdhury@gmail.com>
2026-07-01 20:25:31 -0700

    Added new bucketing mode which produces flat layers instead of gradients

--- rgb2kmeans.py
+++ rgb2kmeans.py
@@ -4,6 +4,19 @@ import numpy as np
 import sklearn
 import math
 from skimage import color
+from enum import Enum
+
+class Mode(Enum):
+    Gradient = 0
+    Bucket   = 1
+
+class Options:
+    def __init__(self, k: int, mode: Mode, alpha: float = 1., invert: bool = False):
+        self.k = k
+        self.mode = mode
+        if self.mode == Mode.Gradient:
+            self.alpha = alpha
+            self.invert = invert
 
 def load_image(path):
     img = PIL.Image.open(path)
@@ -32,7 +45,7 @@ def save_image(layer, path):
     img = PIL.Image.fromarray(np.squeeze(img, axis=-1))
     img.save(path)
 
-def rgb2kmeans(input_path, num_buckets, alpha, invert, output_prefix):
+def rgb2kmeans(input_path, options: Options, output_prefix):
     img = load_image(input_path)
     img = (img / 255.).astype(np.float32)
     # Use LAB color space, as that maps better to Euclidean distance than RGB 
@@ -41,31 +54,38 @@ def rgb2kmeans(input_path, num_buckets, alpha, invert, output_prefix):
     # plt.imshow(rgb2)
     # plt.show()
     pixels = lab.reshape(-1, 3)
-    model = sklearn.cluster.KMeans(n_clusters=num_buckets, random_state=0).fit(pixels)
+    model = sklearn.cluster.KMeans(n_clusters=options.k, random_state=0).fit(pixels)
     centroids = model.cluster_centers_
     white = np.array([1., 1., 1.])
     white_lab = np.array([100., 0.01, -0.01])
     # print(f'Centroids: {centroids}')
-    # buckets = model.predict(pixels)
     # print(f'Buckets: {buckets}')
     # clamped = np.take(centroids, buckets, axis=0)
     layers = []
-    for i in range(num_buckets):
-        l = distance(pixels, centroids[i])
-        # In LAB colorspace the distance isn't in the range 0:1 so it's not suitable for interpolation
-        # Divide by an arbitrary max value to "normalize"
-        l /= 200.
-        l = np.clip(l * alpha, 0, 1)
-        if invert:
-            l = 1 - l
-        layers.append(l.reshape((img.shape[0], img.shape[1], 1)))
+    if options.mode == Mode.Gradient:
+        for i in range(options.k):
+            l = distance(pixels, centroids[i])
+            # In LAB colorspace the distance isn't in the range 0:1 so it's not suitable for interpolation
+            # Divide by an arbitrary max value to "normalize"
+            l /= 200.
+            l = np.clip(l * options.alpha, 0, 1)
+            if options.invert:
+                l = 1 - l
+            layers.append(l.reshape((img.shape[0], img.shape[1], 1)))
+    elif options.mode == Mode.Bucket:
+        labels = model.predict(pixels)
+        for i in range(options.k):
+            l = np.where(labels == i, 0, 1).astype(np.float32)
+            layers.append(l.reshape((img.shape[0], img.shape[1], 1)))
+    else:
+        raise ValueError(f'Unexpected value for mode {options.mode}')
     # Display layers in grid
-    cols = math.ceil(math.sqrt(num_buckets))
-    rows = math.ceil(num_buckets / cols)
+    cols = math.ceil(math.sqrt(options.k))
+    rows = math.ceil(options.k / cols)
     fig, axes = plt.subplots(rows, cols)
-    axes = axes.flatten() if num_buckets > 1 else [axes]
+    axes = axes.flatten() if options.k > 1 else [axes]
     for i, ax in enumerate(axes):
-        if i < num_buckets:
+        if i < options.k:
             preview = (1 - layers[i]) * centroids[i] + layers[i] * white_lab
             preview_rgb = color.lab2rgb(preview)
             ax.imshow(preview_rgb)
@@ -78,7 +98,7 @@ def rgb2kmeans(input_path, num_buckets, alpha, invert, output_prefix):
     plt.tight_layout()
     plt.show()
     # Save layers
-    for i in range(num_buckets):
+    for i in range(options.k):
         save_image(layers[i], f'{output_prefix}layer_{i + 1}.png')
 
 if __name__=='__main__':
@@ -86,8 +106,13 @@ if __name__=='__main__':
     parser = argparse.ArgumentParser('Convert RGB images to layers using K-means algorithm')
     parser.add_argument('input', help='Input image')
     parser.add_argument('-p', '--prefix', default='./', help='Output path prefix')
-    parser.add_argument('-k', '--buckets', type=int, default=2, help='Number of layers')
+    parser.add_argument('-k', '--num_centroids', type=int, default=2, help='Number of layers')
     parser.add_argument('-i', '--inverse', action='store_true', help='Invert layer weights')
-    parser.add_argument('-a', '--alpha', type=float, default=1., help='Scaling factor for distance function')
+    parser.add_argument('-a', '--alpha', type=float, default=1., help='Scaling factor for distance function. Used with gradient mode.')
+    parser.add_argument('-b', '--bucket', action='store_true', help='Use bucketing mode instead of gradient. Results in distinct layers with flat colors instead of overlapping gradients.')
     args = parser.parse_args()
-    rgb2kmeans(args.input, args.buckets, args.alpha, args.inverse, args.prefix)
+    if args.bucket:
+        options = Options(args.num_centroids, Mode.Bucket)
+    else:
+        options = Options(args.num_centroids, Mode.Gradient, args.alpha, args.inverse)
+    rgb2kmeans(args.input, options, args.prefix)
